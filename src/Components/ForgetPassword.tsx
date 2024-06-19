f7223b3aae944f6a839b3bbedcacc50bf35104d9
pranshu baghel <devloperpranshu@gmail.com>
2024-06-19 15:40:28 +0530

    forget password

--- src/Components/ForgetPassword.tsx
+++ src/Components/ForgetPassword.tsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { TextField, Button, Container, Typography, Box } from '@mui/material';
+// import userEvent from '@testing-library/user-event';
 
 interface ForgetPasswordState {
   email: string;
   showOTP: boolean;
   otp: string;
+  generatedOTP: string;
+  setpassword: string;
+  confirmpassword: string;
+  otpverified: boolean;
+
 }
 
 class ForgetPassword extends Component<{}, ForgetPasswordState> {
@@ -14,7 +20,11 @@ class ForgetPassword extends Component<{}, ForgetPasswordState> {
       email: '',
       showOTP: false,
       otp: '',
-      
+      generatedOTP: '',
+      setpassword: '',
+      confirmpassword: '',
+      otpverified: false,
+
     };
   }
 
@@ -35,11 +45,11 @@ class ForgetPassword extends Component<{}, ForgetPasswordState> {
     if (emailExists) {
       const otp = this.generateOtp();
       console.log("Email matches. Generating OTP:", otp);
-      this.setState({ showOTP: true, otp });
+      this.setState({ showOTP: true, otpverified: false, generatedOTP: otp });
     } else {
       alert('Email does not match!');
     }
-  };
+  }
 
   generateOtp = () => {
     const digits = '0123456789';
@@ -53,12 +63,51 @@ class ForgetPassword extends Component<{}, ForgetPasswordState> {
   };
 
   handleOTPSubmit = () => {
-    const { otp } = this.state;
-    alert(`OTP submitted: ${otp}`);
+    const { otp, generatedOTP } = this.state;
+
+    if (otp === generatedOTP) {
+      alert('OTP matches! You can now proceed to reset your password.');
+      this.setState({ otp: '', otpverified: true ,showOTP:false});
+    } else {
+      alert('Incorrect OTP. Please try again.');
+      this.setState({ otp: '' });
+    }
   };
 
+  handlepasswordreset = () => {
+    const { setpassword, confirmpassword, email } = this.state;
+
+    if (setpassword === confirmpassword) {
+      const data = localStorage.getItem('formData');
+      const values = data ? JSON.parse(data) : [];
+      const Updatedvalues = values.map((user: { email: string, name: string, password: string }) => {
+        if (user.email === email) {
+          return { ...user, password: setpassword };
+        }
+        return user;
+      });
+
+      localStorage.setItem('formData', JSON.stringify(Updatedvalues));
+
+      alert("password reset succesfully");
+      this.setState({
+        email: '',
+        showOTP: false,
+        otp: '',
+        generatedOTP: '',
+        otpverified: false,
+        setpassword: '',
+        confirmpassword: '',
+
+      })
+    } else {
+      alert("password do not match. pls try again")
+    }
+  }
+ 
+
   render() {
-    const { email, showOTP, otp } = this.state;
+    const { email, showOTP, otp, otpverified, setpassword, confirmpassword } = this.state;
 
     return (
       <Container component="main" maxWidth="xs">
@@ -117,6 +166,43 @@ class ForgetPassword extends Component<{}, ForgetPasswordState> {
               </Button>
             </>
           )}
+
+          {otpverified && (
+            <>
+              <TextField
+                variant='outlined'
+                margin='normal'
+                required fullWidth
+                name='setpassword'
+                id='setpassword'
+                type='password'
+                label="set password"
+                value={setpassword}
+                onChange={this.handleInputChange}
+
+              />
+              <TextField
+                variant='outlined'
+                margin='normal'
+                required fullWidth
+                name='confirmpassword'
+                id='confirmpassword'
+                type='password'
+                label="confirm password"
+                value={confirmpassword}
+                onChange={this.handleInputChange}
+
+              />
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                onClick={this.handlepasswordreset}
+
+              >Submit</Button>
+
+            </>
+          )}
         </Box>
       </Container>
     );
@@ -124,3 +210,209 @@ class ForgetPassword extends Component<{}, ForgetPasswordState> {
 }
 
 export default ForgetPassword;
+// import { TextField, Button, Container, Typography, Box } from '@mui/material';
+
+// interface ForgetPasswordState {
+//   email: string;
+//   showOTP: boolean;
+//   otp: string;
+//   generatedOTP: string;
+//   setpassword: string;
+//   confirmpassword: string;
+//   otpverified: boolean;
+// }
+
+// class ForgetPassword extends Component<{}, ForgetPasswordState> {
+//   constructor(props: {}) {
+//     super(props);
+//     this.state = {
+//       email: '',
+//       showOTP: false,
+//       otp: '',
+//       generatedOTP: '',
+//       setpassword: '',
+//       confirmpassword: '',
+//       otpverified: false,
+//     };
+//   }
+
+//   handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     const { name, value } = event.target;
+//     this.setState((prevState) => ({
+//       ...prevState,
+//       [name]: value
+//     }));
+//   };
+
+//   handleForgetPassword = () => {
+//     const { email } = this.state;
+//     const data = localStorage.getItem('formData');
+//     const values = data ? JSON.parse(data) : [];
+//     const emailExists = values.some((data: { email: string }) => data.email === email);
+
+//     if (emailExists) {
+//       const otp = this.generateOtp();
+//       console.log("Email matches. Generating OTP:", otp);
+//       this.setState({ showOTP: true, otpverified: false, generatedOTP: otp });
+//     } else {
+//       alert('Email does not match!');
+//     }
+//   }
+
+//   generateOtp = () => {
+//     const digits = '0123456789';
+//     let OTP = '';
+//     const len = digits.length;
+//     for (let i = 0; i < 4; i++) {
+//       OTP += digits[Math.floor(Math.random() * len)];
+//     }
+//     console.log("OTP is", OTP)
+//     return OTP;
+//   };
+
+//   handleOTPSubmit = () => {
+//     const { otp, generatedOTP } = this.state;
+
+//     if (otp === generatedOTP) {
+//       alert('OTP matches! You can now proceed to reset your password.');
+//       this.setState({ otp: '', otpverified: true, showOTP: false });
+//     } else {
+//       alert('Incorrect OTP. Please try again.');
+//       this.setState({ otp: '' });
+//     }
+//   };
+
+//   handlePasswordReset = () => {
+//     const { setpassword, confirmpassword, email } = this.state;
+
+//     if (setpassword === confirmpassword) {
+//       const data = localStorage.getItem('formData');
+//       const values = data ? JSON.parse(data) : [];
+//       const updatedValues = values.map((user: { email: string, name: string, password: string }) => {
+//         if (user.email === email) {
+//           return { ...user, password: setpassword };
+//         }
+//         return user;
+//       });
+
+//       localStorage.setItem('formData', JSON.stringify(updatedValues));
+
+//       alert("Password reset successfully");
+//       this.setState({
+//         email: '',
+//         showOTP: false,
+//         otp: '',
+//         generatedOTP: '',
+//         otpverified: false,
+//         setpassword: '',
+//         confirmpassword: '',
+//       });
+//     } else {
+//       alert("Passwords do not match. Please try again.");
+//     }
+//   }
+
+//   render() {
+//     const { email, showOTP, otp, otpverified, setpassword, confirmpassword } = this.state;
+
+//     return (
+//       <Container component="main" maxWidth="xs">
+//         <Box
+//           display="flex"
+//           flexDirection="column"
+//           alignItems="center"
+//           justifyContent="center"
+//           minHeight="100vh"
+//           marginTop="5rem"
+//         >
+//           <Typography component="h1" variant="h5">
+//             Forget Password
+//           </Typography>
+//           <TextField
+//             variant="outlined"
+//             margin="normal"
+//             required
+//             fullWidth
+//             id="email"
+//             label="Enter your email"
+//             name="email"
+//             autoComplete="email"
+//             value={email}
+//             onChange={this.handleInputChange}
+//           />
+//           <Button
+//             fullWidth
+//             variant="contained"
+//             color="primary"
+//             onClick={this.handleForgetPassword}
+//           >
+//             Submit
+//           </Button>
+//           {showOTP && (
+//             <Box>
+//               <TextField
+//                 variant="outlined"
+//                 margin="normal"
+//                 required
+//                 fullWidth
+//                 id="otp"
+//                 label="Enter OTP"
+//                 name="otp"
+//                 autoComplete="otp"
+//                 value={otp}
+//                 onChange={this.handleInputChange}
+//               />
+//               <Button
+//                 fullWidth
+//                 variant="contained"
+//                 color="primary"
+//                 onClick={this.handleOTPSubmit}
+//               >
+//                 Verify OTP
+//               </Button>
+//             </Box>
+//           )}
+//           {otpverified && (
+//             <Box>
+//               <TextField
+//                 variant="outlined"
+//                 margin="normal"
+//                 required
+//                 fullWidth
+//                 name="setpassword"
+//                 label="Set Password"
+//                 type="password"
+//                 id="setpassword"
+//                 value={setpassword}
+//                 onChange={this.handleInputChange}
+//               />
+//               <TextField
+//                 variant="outlined"
+//                 margin="normal"
+//                 required
+//                 fullWidth
+//                 name="confirmpassword"
+//                 label="Confirm Password"
+//                 type="password"
+//                 id="confirmpassword"
+//                 value={confirmpassword}
+//                 onChange={this.handleInputChange}
+//               />
+//               <Button
+//                 fullWidth
+//                 variant="contained"
+//                 color="primary"
+//                 onClick={this.handlePasswordReset}
+//               >
+//                 Reset Password
+//               </Button>
+//             </Box>
+//           )}
+//         </Box>
+//       </Container>
+//     );
+//   }
+// }
+
+// export default ForgetPassword;
+
--- src/Components/Registration.tsx
+++ src/Components/Registration.tsx
@@ -39,7 +39,7 @@ class Registration extends Component<{}, LoginState> {
       this.setState({ values, submitted: true });
     }
   }
-  
+
   getLocalStorageData = (): Formdata[] => {
     const data = localStorage.getItem('formData');
     return data ? JSON.parse(data) : [];
@@ -47,16 +47,19 @@ class Registration extends Component<{}, LoginState> {
 
   handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    this.setState({...this.state ,[name] : value });
+    this.setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
 
   validateForm = (): boolean => {
     const { name, email, password } = this.state;
     const errors: { [key: string]: string } = {};
 
-    if (!name) errors.name = "Name is required";
-    if (!email) errors.email = "Email is required";
-    if (!password) errors.password = "Password is required";
+    if (!name) errors.name = 'Name is required';
+    if (!email) errors.email = 'Email is required';
+    if (!password) errors.password = 'Password is required';
 
     this.setState({ errors });
     return Object.keys(errors).length === 0;
@@ -76,21 +79,22 @@ class Registration extends Component<{}, LoginState> {
     );
 
     if (dataExists) {
-      alert("This data already exists");
+      alert('This data already exists');
       return;
     }
 
+    const updatedValues = [...values, newdata];
     this.setState(
-      (prevState) => ({
-        values: [...prevState.values, newdata],
+      {
+        values: updatedValues,
         submitted: true,
         name: '',
         email: '',
         password: '',
         errors: {},
-      }),
+      },
       () => {
-        localStorage.setItem('formData', JSON.stringify(this.state.values));
+        localStorage.setItem('formData', JSON.stringify(updatedValues));
       }
     );
   };
@@ -178,238 +182,4 @@ class Registration extends Component<{}, LoginState> {
 }
 
 export default Registration;
-// import React, { Component } from 'react';
-// import { TextField, Button, Container, Typography, Box } from '@mui/material';
 
-// interface Formdata {
-//   name: string;
-//   email: string;
-//   password: string;
-// }
-
-// interface RegistrationState {
-//   name: string;
-//   email: string;
-//   password: string;
-//   submitted: boolean;
-//   values: Formdata[];
-//   errors: {
-//     name?: string;
-//     email?: string;
-//     password?: string;
-//   };
-//   forgetEmail: string;
-//   forgetEmailMatched: boolean;
-// }
-
-// class Registration extends Component<{}, RegistrationState> {
-//   constructor(props: {}) {
-//     super(props);
-//     this.state = {
-//       name: '',
-//       email: '',
-//       password: '',
-//       submitted: false,
-//       values: this.getLocalStorageData(),
-//       errors: {},
-//       forgetEmail: '',
-//       forgetEmailMatched: false,
-//     };
-//   }
-
-//   componentDidMount() {
-//     const values = this.getLocalStorageData();
-//     if (values.length > 0) {
-//       this.setState({ values, submitted: true });
-//     }
-//   }
-
-//   getLocalStorageData = (): Formdata[] => {
-//     const data = localStorage.getItem('formData');
-//     return data ? JSON.parse(data) : [];
-//   };
-
-//   handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     const { name, value } = event.target;
-//     this.setState((prevState) => ({
-//       ...prevState,
-//       [name]: value,
-//     }));
-//   };
-
-//   validateForm = (): boolean => {
-//     const { name, email, password } = this.state;
-//     const errors: { [key: string]: string } = {};
-
-//     if (!name) errors.name = 'Name is required';
-//     if (!email) errors.email = 'Email is required';
-//     if (!password) errors.password = 'Password is required';
-
-//     this.setState({ errors });
-//     return Object.keys(errors).length === 0;
-//   };
-
-//   handleSubmit = (event: React.FormEvent) => {
-//     event.preventDefault();
-//     if (!this.validateForm()) {
-//       return;
-//     }
-
-//     const { name, email, password, values } = this.state;
-//     const newdata: Formdata = { name, email, password };
-
-//     const dataExists = values.some(
-//       (data) => data.name === name && data.email === email && data.password === password
-//     );
-
-//     if (dataExists) {
-//       alert('This data already exists');
-//       return;
-//     }
-
-//     this.setState(
-//       (prevState) => ({
-//         values: [...prevState.values, newdata],
-//         submitted: true,
-//         name: '',
-//         email: '',
-//         password: '',
-//         errors: {},
-//       }),
-//       () => {
-//         localStorage.setItem('formData', JSON.stringify(this.state.values));
-//       }
-//     );
-//   };
-
-//   handleForgetPassword = () => {
-//     const { forgetEmail, values } = this.state;
-//     const dataExists = values.some((data) => data.email === forgetEmail);
-
-//     if (dataExists) {
-//       alert('Email matched!'); // Replace with your forget password logic
-//       this.setState({ forgetEmailMatched: true });
-//     } else {
-//       alert('Email does not match!');
-//       this.setState({ forgetEmailMatched: false });
-//     }
-//   };
-
-//   render() {
-//     const { name, email, password, errors, submitted, values, forgetEmail, forgetEmailMatched } = this.state;
-
-//     return (
-//       <Container component="main" maxWidth="xs">
-//         <Box
-//           display="flex"
-//           flexDirection="column"
-//           alignItems="center"
-//           justifyContent="center"
-//           minHeight="100vh"
-//           marginTop="5rem"
-//         >
-//           <Typography component="h1" variant="h5">
-//             Registration Form
-//           </Typography>
-//           <form onSubmit={this.handleSubmit} noValidate>
-//             <TextField
-//               variant="outlined"
-//               margin="normal"
-//               required
-//               fullWidth
-//               id="name"
-//               label="Name"
-//               name="name"
-//               autoComplete="name"
-//               autoFocus
-//               value={name}
-//               onChange={this.handleInputChange}
-//               error={!!errors.name}
-//               helperText={errors.name}
-//             />
-//             <TextField
-//               variant="outlined"
-//               margin="normal"
-//               required
-//               fullWidth
-//               id="email"
-//               label="Email"
-//               name="email"
-//               autoComplete="email"
-//               value={email}
-//               onChange={this.handleInputChange}
-//               error={!!errors.email}
-//               helperText={errors.email}
-//             />
-//             <TextField
-//               variant="outlined"
-//               margin="normal"
-//               required
-//               fullWidth
-//               name="password"
-//               label="Password"
-//               type="password"
-//               id="password"
-//               autoComplete="current-password"
-//               value={password}
-//               onChange={this.handleInputChange}
-//               error={!!errors.password}
-//               helperText={errors.password}
-//             />
-//             <Button type="submit" fullWidth variant="contained" color="primary">
-//               Submit
-//             </Button>
-//           </form>
-
-//           {/* Forget Password Section */}
-//           <Box mt={4}>
-//             <Typography component="h2" variant="h6">
-//               Forget Password
-//             </Typography>
-//             <TextField
-//               variant="outlined"
-//               margin="normal"
-//               required
-//               fullWidth
-//               id="forgetEmail"
-//               label="Enter your email"
-//               name="forgetEmail"
-//               autoComplete="email"
-//               value={forgetEmail}
-//               onChange={this.handleInputChange}
-//             />
-//             <Button
-//               fullWidth
-//               variant="contained"
-//               color="primary"
-//               onClick={this.handleForgetPassword}
-//             >
-//               Submit
-//             </Button>
-//             {forgetEmailMatched && (
-//               <Typography color="primary" mt={2}>
-//                 Email matched! Proceed with password reset logic.
-//               </Typography>
-//             )}
-//           </Box>
-
-//           {/* Submitted Data Section */}
-//           {submitted && (
-//             <Box mt={4}>
-//               {values.map((data, index) => (
-//                 <Box key={index} my={1}>
-//                   <Typography>Full Name: {data.name}</Typography>
-//                   <Typography>Email: {data.email}</Typography>
-//                   <Typography>Password: {data.password}</Typography>
-//                 </Box>
-//               ))}
-//             </Box>
-//           )}
-//         </Box>
-//       </Container>
-//     );
-//   }
-// }
-
-// export default Registration;
- 
